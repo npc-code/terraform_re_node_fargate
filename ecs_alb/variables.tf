c6f4bfe6b4cc802b93fb54a95e72c342201db3ff
Nic <nic.ceynowa@gmail.com>
2021-03-17 08:56:59 -0400

    additional cleanup

--- ecs_alb/variables.tf
+++ ecs_alb/variables.tf
@@ -10,6 +10,11 @@ variable "container_port" {
   description = "ALB target port"
 }
 
+variable "container_subnets" {
+    type = list(string)
+    description = "private subnets for containers"
+}
+
 variable "desired_task_cpu" {
   description = "Task CPU Limit"
 }
@@ -18,22 +23,32 @@ variable "desired_task_memory" {
   description = "Task Memory Limit"
 }
 
+variable "desired_tasks" {
+  description = "Number of containers desired to run the application task"
+}
+
+variable "domain_name" {
+    description = "root domain for route53 hosted zone"
+    default = ""
+}
+
+variable "ecr_repo_name" {
+    description = "Name of the ecr repo to use"
+}
+
 variable "environment_variables" {
   type        = map(string)
   description = "ecs task environment variables"
 }
 
-variable "alb_port" {
-  description = "ALB listener port"
-}
-
-variable "vpc_id" {
-  description = "The VPC id"
-}
-
 variable "external_ip" {
   type    = string
-  default = "74.69.167.125/32"
+  default = "0.0.0.0/0"
+}
+
+variable "health_check_path" {
+  description = ""
+  default     = "/"
 }
 
 variable "region" {
@@ -46,33 +61,13 @@ variable "subnets" {
   description = "subnets to use"
 }
 
-variable "container_subnets" {
-    type = list(string)
-    description = "private subnets for containers"
-}
-
-variable "health_check_path" {
-  description = ""
-  default     = "/"
-}
-
-variable "desired_tasks" {
-  description = "Number of containers desired to run the application task"
-}
-
-variable "ecr_repo_name" {
-    description = "Name of the ecr repo to use"
-}
-
-variable "domain_name" {
-    description = "root domain for route53 hosted zone"
-    default = ""
-}
-
 variable "url" {
     type = string
     description = "domain name to be used for the alb"
     default = ""
 }
 
+variable "vpc_id" {
+  description = "The VPC id"
+}
 
